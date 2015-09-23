af26e365037357cc021b5337be6c29faed987cd9
solankepl <pavanso@cybage.com>
2015-09-23 11:01:06 +0530

    Commiting in master

---
+++ navigation/left_menu.php
@@ -0,0 +1,5 @@
+<div class="left_menu">
+	<ul class="left_menu_ul">
+		<li><a class="left_link <?php if($page == 'Dashboard'){echo 'selected_left';}?>" href="<?php echo FOLDER_PATH_HTTP;?>dashboard.php">Dashboard</a></li>
+	</ul>
+</div>
